28d47c7875b500c966166b1530486a0d1b5cbcff
Shawn Harrington <sdharrington@comt-fvfcf00jl413.wifi.nic.edu>
2023-05-04 13:51:22 -0700

    init commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,18 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import { Oswald } from "next/font/google";
+import { SessionProvider } from "next-auth/react";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const oswald = Oswald({
+  subsets: ["latin"],
+  weight: ["200", "300", "400", "500", "600", "700"],
+});
+
+export default function App({ session, Component, pageProps }) {
+  return (
+    <SessionProvider session={session}>
+      <div className={oswald.className}>
+        <Component {...pageProps} />
+      </div>
+    </SessionProvider>
+  );
 }
---
+++ src/pages/api/auth/[...nextauth].js
@@ -0,0 +1,36 @@
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import clientPromise from "../../../../lib/mongodb";
+// import AppleProvider from "next-auth/providers/apple";
+// import FacebookProvider from "next-auth/providers/facebook";
+// import EmailProvider from "next-auth/providers/email";
+
+export const authOptions = {
+  // Configure one or more authentication providers
+  providers: [
+    // AppleProvider({
+    //   clientId: process.env.APPLE_ID,
+    //   clientSecret: process.env.APPLE_SECRET,
+    // }),
+    // FacebookProvider({
+    //   clientId: process.env.FACEBOOK_ID,
+    //   clientSecret: process.env.FACEBOOK_SECRET,
+    // }),
+    // GithubProvider({
+    //   clientId: process.env.GITHUB_ID,
+    //   clientSecret: process.env.GITHUB_SECRET,
+    // }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
+    // Emailrovider({
+    //   server: process.env.MAIL_SERVER,
+    //   from: "NextAuth.js <no-reply@example.com>",
+    // }),
+    // ...add more providers here
+  ],
+  adapter: MongoDBAdapter(clientPromise),
+};
+export default NextAuth(authOptions);
