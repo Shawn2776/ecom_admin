0976e2acde42d0169065e1cb8221a5db4d9af62c
Shawn Harrington <sdharrington@comt-fvfcf00jl413.wifi.nic.edu>
2023-05-09 16:47:45 -0700

    removed missing dep

--- src/pages/settings.js
+++ src/pages/settings.js
@@ -1,7 +1,3 @@
-import Layout from "@/components/Layout";
-import Modal from "@/components/Modal/Modal";
-import React from "react";
-
 export default function Settings() {
   return <div>Settings</div>;
 }
